e9336457dfa1008a8a7e4d9cdb7d17823632a43f
Paulo Santos <G00298711@stu.raritanval.edu>
2024-01-22 12:46:02 -0500

    Moved Sun + Started a tree

--- Picture.java
+++ Picture.java
@@ -1,18 +1,21 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
+ * This class draws a house with a person next to a tree.
+ * It makes use of different classes to make the objects drawn on the picture.
+ * 
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author Paulo Santos
+ * @version 2024.01.22
  */
 public class Picture
 {
     private Square wall;
     private Square window;
+    private Square trunk;
+    private Triangle leaf1;
+    private Triangle leaf2;
+    private Triangle leaf3;
     private Triangle roof;
     private Circle sun;
     private boolean drawn;
@@ -26,6 +29,10 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        trunk = new Square();
+        leaf1 = new Triangle();
+        leaf2 = new Triangle();
+        leaf3 = new Triangle();
         drawn = false;
     }
 
@@ -40,20 +47,44 @@ public class Picture
             wall.changeSize(120);
             wall.makeVisible();
             
+            trunk.moveHorizontal(60);
+            trunk.moveVertical(120);
+            trunk.changeSize(20);
+            trunk.makeVisible();
+            trunk.changeColor("brown");
+            
             window.changeColor("black");
             window.moveHorizontal(-120);
             window.moveVertical(40);
             window.changeSize(40);
             window.makeVisible();
-    
+            
+            leaf1.changeColor("green");
+            leaf1.moveHorizontal(60);
+            leaf1.moveVertical(110);
+            leaf1.changeSize(40, 180);
+            leaf1.makeVisible();
+            
+            leaf2.changeColor("green");
+            leaf2.moveHorizontal(60);
+            leaf2.moveVertical(90);
+            leaf2.changeSize(40, 180);
+            leaf2.makeVisible();
+            
+            leaf3.changeColor("green");
+            leaf3.moveHorizontal(60);
+            leaf3.moveVertical(70);
+            leaf3.changeSize(40, 180);
+            leaf3.makeVisible();
+            
             roof.changeSize(60, 180);
             roof.moveHorizontal(20);
             roof.moveVertical(-60);
             roof.makeVisible();
     
             sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
+            sun.moveHorizontal(230);
+            sun.moveVertical(-120);
             sun.changeSize(80);
             sun.makeVisible();
             drawn = true;
@@ -69,6 +100,10 @@ public class Picture
         window.changeColor("white");
         roof.changeColor("black");
         sun.changeColor("black");
+        trunk.changeColor("black");
+        leaf1.changeColor("black");
+        leaf2.changeColor("black");
+        leaf3.changeColor("black");
     }
 
     /**
@@ -80,5 +115,9 @@ public class Picture
         window.changeColor("black");
         roof.changeColor("green");
         sun.changeColor("yellow");
+        trunk.changeColor("brown");
+        leaf1.changeColor("green");
+        leaf2.changeColor("green");
+        leaf3.changeColor("green");
     }
 }
